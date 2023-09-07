fe8925e8a68457fd972928f19c5f68cee870dd5b
Ivan Marfin <ivan.marfin@timebook.ru>
2023-09-07 09:11:56 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="todo-list">
+    <h2 class="title">Список дел</h2>
+    <ul class="todos">
+      <li v-for="todo of todos" :key="todo.id">
+        <input :id="todo.id" type="checkbox" @change="markDone(todo.id)" />
+        <label :for="todo.id" :class="{ '-done': todo.isDone }">
+          {{ todo.name }}
+        </label>
+        <button class="delete-btn" @click="deleteTodo(todo.id)">х</button>
+      </li>
+    </ul>
+    <input
+        type="text"
+        placeholder="Запишите что-то..."
+        v-model="todoName"
+    />
+    <button @click="onClickAdd">Добавить дело</button>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<style scoped>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+.-done {
+  text-decoration: line-through;
+}
+
+.todo-list {
+  margin-left: 150px;
+}
+
+.todos {
+  padding: 0;
+}
+
+.todos > li {
+  list-style: none;
+  display: flex;
+  align-items: center;
+}
+
+.delete-btn {
+  margin-left: 16px;
+  background-color: red;
+  color: white;
+  border: none;
+  border-radius: 50%;
+  width: 16px;
+  height: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+}
+</style>
+
+<script setup>
+
+import {ref} from "vue";
+
+let todos = ref([
+  { id: 1, name: 'Купить пиво', isDone: false },
+  { id: 2, name: 'Купить хлеб', isDone: false },
+  { id: 3, name: 'Пойти спать', isDone: false },
+]);
+
+let todoName = ref('');
+
+function onClickAdd () {
+  todos.value.push({ id: Date.now(), name: todoName.value, isDone: false });
+  todoName.value = '';
+}
+
+function deleteTodo (id) {
+  todos.value = todos.value.filter((todo) => todo.id !== id)
+}
+
+function markDone (id) {
+  const todo = todos.value.find((todo) => todo.id === id)
+  if (todo) {
+    todo.isDone = !todo.isDone;
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
